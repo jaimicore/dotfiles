89651442ab7fddd8c0b475a150e5f708a76d6723
jaimicore <j.a.castro.mondragon@gmail.com>
2025-05-12 09:32:44 +0200

    First commands

---
+++ setup.sh
@@ -0,0 +1,7 @@
+## More commands to be added.
+
+## ----------- ##
+## Basic stuff ##
+## ----------- ##
+alias ls='ls -p' # Adds slash after directory names
+alias lsd='ls -d */' # List directories only
