cf5626d2f714c35aa2d3f6a53acdf8d7ea4d131d
jaimicore <j.a.castro.mondragon@gmail.com>
2025-05-12 09:44:38 +0200

    Added mendeley ; Source bashrc when sourcing zsh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,25 @@
 ## More commands to be added.
 
+
+## ------------------------------------------ ##
+## Source your .bashrc config when useing zsh ##
+## ------------------------------------------ ##
+
+# Exec ~/.bashrc and ~/.profile when using zsh
+# This avoids to have to manually load zsh and bashrc config files
+if [ -f '~/.profile' ]; then; source '~/.profile'; fi;
+source <(awk '{ if(NR>118)print}' ~/.bashrc)  
+# Line 118 is works for Ubuntu's default .bashrc
+
+
 ## ----------- ##
 ## Basic stuff ##
 ## ----------- ##
 alias ls='ls -p' # Adds slash after directory names
 alias lsd='ls -d */' # List directories only
+
+
+## ------------------------------ ##
+## Use Mendeley reference manager ##
+## ------------------------------ ##
+alias mendel='flatpak run com.elsevier.MendeleyDesktop'
